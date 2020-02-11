ce5d84e15b93133290959f8cc704498bb1e4b122
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-11 21:12:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,11 +64,6 @@ def dennis_ritchies_language
     end
 
 def adding_matz
-  programmer_hash[:yukihiro_matsumoto] = {
-      :known_for => "Ruby",
-       :languages => ["LISP", "C"]
-    }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -85,7 +80,11 @@ def adding_matz
         }
         
      }
-
+  
+  programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+    }
 
     
 end
